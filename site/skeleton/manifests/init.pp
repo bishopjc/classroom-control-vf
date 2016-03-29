05f6cb39dc3d78342db4cef7adf116740c1ecfba
bishopjc <john.c.bishop@intel.com>
2016-03-29 10:41:00 -0700

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,1 +1,14 @@
+class skeleton {
 
+   file {
+    "/etc/skel":
+    ensure => directory,
+   }
+   
+   file {
+    "/etc/skel/.bashrc":
+    ensure => file,
+    source => "/site/skel/manifests/.bashrc",
+   }
+
+}
